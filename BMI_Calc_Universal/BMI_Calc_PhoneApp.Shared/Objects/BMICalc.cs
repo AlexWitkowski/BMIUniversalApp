feb8eb09bc5d989e6e7e46cb1c2184c5239d2554
Alex Witkowski <alex.witkowski99@live.com>
2014-12-04 05:11:55 -0500

    Universal BMI
    testing universal applications for windows

---
+++ BMI_Calc_Universal/BMI_Calc_PhoneApp.Shared/Objects/BMICalc.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Popups;
+
+namespace BMI_Calc_PhoneApp.Classes
+{
+    class BMICalc
+    {
+        string height1;
+        string height2;
+        string weight;
+        bool? isImperial;
+        string result;
+        string resultDesc;
+
+        public BMICalc(string BMIHeight1, string BMIHeight2, string BMIWeight, bool? BMIIsImperial)
+        {
+            
+                this.height1 = BMIHeight1;
+                this.height2 = BMIHeight2;
+                this.weight = BMIWeight;
+                this.isImperial = BMIIsImperial;
+            try
+            {
+                if (weight == "")
+                {
+                    weight = "0";
+                }
+                if (height1 == "")
+                {
+                    height1 = "0";
+                }
+                if (height2 == "")
+                {
+                    height2 = "0";
+                }
+
+                double dubWeight;
+                double dubHeight;
+                double dubResult;
+
+                if (isImperial == true)
+                {
+                    dubWeight = (Convert.ToDouble(weight));
+                    dubHeight = (Convert.ToDouble(height1) + (Convert.ToDouble(height2) / 100));
+                }
+                else
+                {
+                    dubWeight = (Convert.ToDouble(weight) / 2.2046);
+                    dubHeight = ((((Convert.ToDouble(height1) * 12) + (Convert.ToDouble(height2))) * 2.54) / 100);
+                }
+
+                dubResult = Math.Round((dubWeight / (Math.Pow(dubHeight, 2))), 3);
+                this.result = dubResult.ToString();
+
+                if (dubResult < 15)
+                {
+                    this.resultDesc = "Very Severly Underweight";
+                }
+                else if (dubResult >= 15 && dubResult < 16)
+                {
+                    this.resultDesc = "Severly Underweight";
+                }
+                else if (dubResult >= 16 && dubResult < 18.5)
+                {
+                    this.resultDesc = "Underweight";
+                }
+                else if (dubResult >= 18.5 && dubResult < 25)
+                {
+                    this.resultDesc = "Normal (Healthy Weight)";
+                }
+                else if (dubResult >= 25 && dubResult < 30)
+                {
+                    this.resultDesc = "Overweight";
+                }
+                else if (dubResult >= 30 && dubResult < 35)
+                {
+                    this.resultDesc = "Obese Class 1 (Moderately Obese)";
+                }
+                else if (dubResult >= 35 && dubResult < 40)
+                {
+                    this.resultDesc = "Obese Class 2 (Severly Obese)";
+                }
+                else if (dubResult >= 40)
+                {
+                    this.resultDesc = "Obese Class 3 (Very Severly Obese)";
+                }
+                else
+                {
+                    this.resultDesc = "An Error has Occured.";
+                    this.result = "result is not a number.";
+                }
+            }
+            catch (Exception se)
+            {
+                this.result = se.Message;
+                this.resultDesc = "An Error has Occured.";
+            }   
+        }
+        public string Result { get { return result; } }
+        public string ResultDesc { get { return resultDesc; } }
+
+    }
+}
---
+++ BMI_Calc_Universal/BMI_Calc_PhoneApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using BMI_Calc_PhoneApp.Classes;
+using System.Text.RegularExpressions;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BMI_Calc_PhoneApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            if (chkConvert.IsChecked == true)
+            {
+                lblWeightDesc.Text = "Kilograms";
+                lblHeight1Desc.Text = "Meters";
+                lblHeight2Desc.Text = "Centimeters";
+            }
+            else
+            {
+                lblWeightDesc.Text = "Pounds";
+                lblHeight1Desc.Text = "Feet";
+                lblHeight2Desc.Text = "Inches";
+            }
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lblBMINumber.Text = "";
+                lblBMIDescription.Text = "";
+
+                BMICalc yourBMI = new BMICalc(txtHeight1.Text, txtHeight2.Text, txtWeight.Text, chkConvert.IsChecked);
+
+
+                lblBMINumber.Text = yourBMI.Result;
+
+                lblBMIDescription.Text = yourBMI.ResultDesc;
+
+                lblResults.Visibility = Visibility.Visible;
+                lblBMINumber.Visibility = Visibility.Visible;
+                lblBMIDescription.Visibility = Visibility.Visible;
+
+            }
+            catch (Exception se)
+            {
+                lblResults.Visibility = Visibility.Visible;
+                lblBMINumber.Visibility = Visibility.Visible;
+                lblBMINumber.Text = se.Message;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtHeight1.Text = "";
+            txtHeight2.Text = "";
+            txtWeight.Text = "";
+            lblBMIDescription.Text = "";
+            lblBMINumber.Text = "";
+
+            lblResults.Visibility = Visibility.Collapsed;
+            lblBMINumber.Visibility = Visibility.Collapsed;
+            lblBMIDescription.Visibility = Visibility.Collapsed;
+        }
+
+        private void chkConvert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Regex rgx = new Regex(@"^\d+.?\d*$");
+
+                if (chkConvert.IsChecked == true)
+                {
+                    lblWeightDesc.Text = "Kilograms";
+                    lblHeight1Desc.Text = "Meters";
+                    lblHeight2Desc.Text = "Centimeters";
+
+                    if (rgx.IsMatch(txtWeight.Text))
+                    {
+                        txtWeight.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtWeight.Text) / 2.2046, 3));
+                    }
+
+                    if (rgx.IsMatch(txtHeight1.Text))
+                    {
+                        txtHeight1.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtHeight1.Text) / 3.2808, 3));
+                    }
+
+                    if (rgx.IsMatch(txtHeight2.Text))
+                    {
+                        txtHeight2.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtHeight2.Text) * 2.54, 3));
+                    }
+                }
+                else
+                {
+                    lblWeightDesc.Text = "Pounds";
+                    lblHeight1Desc.Text = "Feet";
+                    lblHeight2Desc.Text = "Inches";
+
+                    if (rgx.IsMatch(txtWeight.Text))
+                    {
+                        txtWeight.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtWeight.Text) * 2.2046, 3));
+                    }
+
+                    if (rgx.IsMatch(txtHeight1.Text))
+                    {
+                        txtHeight1.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtHeight1.Text) * 3.2808, 3));
+                    }
+
+                    if (rgx.IsMatch(txtHeight2.Text))
+                    {
+                        txtHeight2.Text = Convert.ToString(Math.Round(Convert.ToDouble(txtHeight2.Text) / 2.54, 3));
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
